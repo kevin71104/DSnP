faf63e469833039b11d329e8f8730492881f6b28
kevin71104 <b03901026@ntu.edu.tw>
2017-01-16 16:08:51 +0800

    myHashMap

--- fraig/src/util/myHashMap.h
+++ fraig/src/util/myHashMap.h
@@ -39,7 +39,8 @@ class HashMap
 typedef pair<HashKey, HashData> HashNode;
 
 public:
-   HashMap(size_t b=0) : _numBuckets(0), _buckets(0) { if (b != 0) init(b); }
+   HashMap() : _numBuckets(0), _buckets(0) {}
+   HashMap(size_t b=0) : _numBuckets(0), _buckets(0) { init(b); }
    ~HashMap() { reset(); }
 
    // [Optional] TODO: implement the HashMap<HashKey, HashData>::iterator
@@ -56,47 +57,37 @@ public:
       friend class HashMap<HashKey, HashData>;
 
    public:
-      iterator(bool begin, size_t num, vector<HashNode>* b)
-            :index(0), pos(0), _numBuckets(num), _buckets(b){
-           if(begin){
-               for(;index < _numBuckets; index ++)
-                   if(!_buckets[index].empty()){
-                       break;
-                   }
-           }
-           else index = _numBuckets; //iterator end
-       }
+      iterator(HashMap<HashKey, HashData>* h=0, size_t b=0,size_t bn=0 )
+            :_hash(h), _bId(b), _bnId(bn){}
+      iterator(const iterator& i): _hash(i._hash), _bId(i._bId), _bnId(i._bnId) {}
        ~iterator(){}
 
-       HashNode& operator * () const{ return _buckets[index][pos];}
-       HashNode& operator *() { return _buckets[index][pos];}
+       //*_hash[_bId] is vector<HashNode>
+       const HashNode& operator * () const{ return (*_hash)[_bId][_bnId];}
+       HashNode& operator *() { return (*_hash)[_bId][_bnId];}
 
        //prefix
        iterator& operator ++ (){
-           if(index == _numBuckets) return (*this);
-           if(_buckets[index].size()-1 > pos )
-               pos ++;
-           else{
-               for(++index; index < _numBuckets; ++index)
-                   if(!_buckets[index].empty()) break;
-               pos = 0;
-           }
-           return (*this);
+         if( _hash == 0) return (*this);
+         if(_bId >= _hash->_numBuckets) return (*this);
+         if (++_bnId >= (*_hash)[_bId].size()) {
+           while ((++_bId < _hash->_numBuckets) && (*_hash)[_bId].empty());
+           _bnId = 0;
+         }
+         return (*this);
        }
 
        iterator& operator -- (){
-           if(index == 0 && pos == 0) return (*this);
-           if( pos > 0 )
-               pos --;
-           else
-               for(--index; index >= 0 ;--index){
-                   if(!_buckets[index].empty()){
-                       pos = _buckets[index].size()-1;
-                       break;
-                   }
-                   if (index == 0) break;
-               }
-           return (*this);
+         if (_hash == 0) return (*this);
+         if (_bnId == 0) {
+           if (_bId == 0) return (*this);
+           while ((*_hash)[--_bId].empty())
+             if (_bId == 0) return (*this);
+           _bnId = (*_hash)[_bId].size() - 1;
+         }
+         else
+            --_bnId;
+         return (*this);
        }
 
        //postfix
@@ -112,34 +103,30 @@ public:
        }
 
        iterator& operator = (const iterator& i){
-           index = i.index;
-           pos = i.pos;
-           _numBuckets = i._numBuckets;
-           _buckets = i._buckets;
+           _bId = i._bId;
+           _bnId = i._bnId;
+           _hash = i._hash;
+           return (*this);
        }
        bool operator == (const iterator& i){
-           return ((pos == i.pos) && (index == i.index));
+           return (_hash == i._hash && _bId == i._bId && _bnId == i._bnId);
        }
        bool operator != (const iterator& i){
-           return ((pos != i.pos) || (index != i.index));
+           return ( !(*this) == i);
        }
 
    private:
-       size_t                   index;
-       size_t                   pos; //_buckets[index][pos]
-       const size_t             _numBuckets;
-       vector<HashNode>*        _buckets;
+       size_t                       _bId;
+       size_t                       _bnId; //(_hash*)[_bId][_bnId]
+       HashMap<HashKey, HashData>*  _hash;
    };
 
-   void init(size_t b) {
-      reset(); _numBuckets = b; _buckets = new vector<HashNode>[b]; }
+   void init(size_t b) { reset(); _numBuckets = b; _buckets = new vector<HashNode>[b]; }
    void reset() {
       _numBuckets = 0;
       if (_buckets) { delete [] _buckets; _buckets = 0; }
    }
-   void clear() {
-      for (size_t i = 0; i < _numBuckets; ++i) _buckets[i].clear();
-   }
+
    size_t numBuckets() const { return _numBuckets; }
 
    vector<HashNode>& operator [] (size_t i) { return _buckets[i]; }
@@ -148,31 +135,54 @@ public:
    // TODO: implement these functions
    //
    // Point to the first valid data
-   iterator begin() const { return iterator(true,_numBuckets,_buckets); }
+   iterator begin() const {
+     if (_buckets == 0) return end();
+     for (size_t i = 0; i < _numBuckets; ++i)
+       if (!_buckets[i].empty())
+          return iterator(const_cast< HashMap<HashKey, HashData>* >(this), i, 0);
+     return end();
+   }
    // Pass the end
-   iterator end() const { return iterator(false,_numBuckets,_buckets); }
+   iterator end() const { return iterator(const_cast<HashMap<HashKey, HashData>*>(this),_numBuckets, 0);}
+   
    // return true if no valid data
    bool empty() const {
-       bool flag = true;
-       for(size_t i=0; i < _buckets.size(); i++)
-           if(!_buckets[i].empty()){
-               flag = false;
-               break;
-           }
-       return flag;
+      for(size_t i=0; i < _numBuckets; i++)
+           if(!_buckets[i].empty()) return false;
+      return true;
    }
    // number of valid data
-   size_t size() const { size_t s = 0; return s; }
+   size_t size() const {
+     size_t s = 0;
+     for (size_t i = 0; i < _numBuckets; ++i) s += _buckets[i].size();
+     return s;
+   }
 
    // check if k is in the hash...
    // if yes, return true;
    // else return false;
-   bool check(const HashKey& k) const { return false; }
+   bool check(const HashKey& k, HashData& n) const {
+     size_t b = bucketNum(k);
+     for (size_t i = 0; i < _buckets[b].size(); i++)
+       if (_buckets[b][i].first == k) {
+          n = _buckets[b][i].second;
+          return true;
+       }
+     return false;
+   }
 
    // query if k is in the hash...
    // if yes, replace d with the data in the hash and return true;
    // else return false;
-   bool query(const HashKey& k, HashData& d) const { return false; }
+   bool query(const HashKey& k, HashData& d) const {
+     size_t b = bucketNum(k);
+     for (size_t i = 0; i < _buckets[b].size(); i++)
+       if (_buckets[b][i].first == k) {
+          d = _buckets[b][i].second;
+          return true;
+       }
+     return false;
+   }
 
    // update the entry in hash that is equal to k (i.e. == return true)
    // if found, update that entry with d and return true;
@@ -181,7 +191,33 @@ public:
 
    // return true if inserted d successfully (i.e. k is not in the hash)
    // return false is k is already in the hash ==> will not insert
-   bool insert(const HashKey& k, const HashData& d) { return true; }
+   bool insert(const HashKey& k, const HashData& d) {
+     size_t b = bucketNum(k);
+     for (size_t i = 0; i < _buckets[b].size(); i++)
+       if (_buckets[b][i].first == k) {
+          return false;
+       }
+     _buckets[b].push_back(HashNode(k, d));
+     return true;
+   }
+
+   // return true if inserted successfully (i.e. k is not in the hash)
+   // return false is k is already in the hash ==> still do the insertion
+   bool replaceInsert(const HashKey& k, const HashData& d) {
+      size_t b = bucketNum(k);
+      for (size_t i = 0; i < _buckets[b].size(); i++)
+         if (_buckets[b][i].first == k) {
+            _buckets[b][i].second = d;
+            return false;
+         }
+      _buckets[b].push_back(HashNode(k, d));
+      return true;
+   }
+
+   // Need to be sure that k is not in the hash
+   void forceInsert(const HashKey& k, const HashData& d) {
+      _buckets[bucketNum(k)].push_back(HashNode(k, d));
+    }
 
    // return true if removed successfully (i.e. k is in the hash)
    // return fasle otherwise (i.e. nothing is removed)
